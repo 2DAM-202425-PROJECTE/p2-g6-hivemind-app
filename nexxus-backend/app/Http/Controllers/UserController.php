0bcdf024a4f0db1f24ee3e2d93ba9298a4c4e97e
gerardmoreno123 <gerardmoreno@iesebre.com>
2025-01-20 19:11:48 +0100

    Add show json respone for users API

--- nexxus-backend/app/Http/Controllers/UserController.php
+++ nexxus-backend/app/Http/Controllers/UserController.php
@@ -7,9 +7,15 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(): \Illuminate\Http\JsonResponse
     {
         $users = User::all();
         return response()->json($users);
+    }
+
+    public function show($id): \Illuminate\Http\JsonResponse
+    {
+        $user = User::find($id);
+        return response()->json($user);
     }
 }
