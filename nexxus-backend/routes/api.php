11e86a02ffa66dfc58ca60ee44917a22bbe3827e
gerardmoreno123 <gerardmoreno@iesebre.com>
2025-02-03 19:26:47 +0100

    New dashboard, add user CRUD admin panel

--- nexxus-backend/routes/api.php
+++ nexxus-backend/routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
 
 Route::middleware('auth:sanctum')->group(function ()
 {
@@ -14,3 +16,4 @@ Route::middleware('auth:sanctum')->group(function ()
     });
     Route::post('/posts', [PostController::class, 'store']);
 });
+
